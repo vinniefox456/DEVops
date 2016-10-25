dceea77a5f40fb2eea1b749ec2b911c4284455f7
vinniefox456 <vincentgeerts_456@hotmail.com>
2016-10-25 10:22:12 +0200

    INIT COMMIT
    test

---
+++ DEVops/DEVops/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DEVops.Startup))]
+namespace DEVops
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
